5a6e91cc26ea05c61893189e9821665d423e19ed
Cisco89 <franciscoadriangarcia1@gmail.com>
2018-03-07 04:29:23 -0700

    Continuation of funtionality to index.jsx

--- Cisco/src/index.jsx
+++ Cisco/src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+import { App } from './components/App.jsx';
 
 render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('cisco') // points back to <div id="cisco"></div>
 );
